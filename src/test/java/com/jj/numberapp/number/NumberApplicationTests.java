d33d3fc8175be83b19a296bfe468e0526fbd506a
Justin Johnson <jjustin634@gmail.com>
2019-01-18 11:30:04 -0600

    first commit

---
+++ src/test/java/com/jj/numberapp/number/NumberApplicationTests.java
@@ -0,0 +1,17 @@
+package com.jj.numberapp.number;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NumberApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
